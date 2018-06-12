b7d60b36aabb81207fa7f92e5b11d0c671aab367
Davide Tampellini <tampe125@gmail.com>
2018-06-12 16:48:02 +0200

    Working on repo structure

---
+++ build/templates/link.php
@@ -0,0 +1,5 @@
+<?php
+// Internal linking script
+$hardlink_files  = [];
+$symlink_files   = [];
+$symlink_folders = [];
---
+++ build/templates/version.php
@@ -0,0 +1,6 @@
+<?php
+// Protect from unauthorized access
+defined('_JEXEC') or die;
+
+define('TESTIMONIALS_VERSION', '##VERSION##');
+define('TESTIMONIALS_DATE', '##DATE##');
